1fa7c6670fad8523aff04f01280eb99993fefda5
Vipin Singh Bagri <vipinbagri541@gmail.com>
2026-06-12 16:28:24 +0530

    Fix CI build error: Remove secrets plugin and restore dummy env file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
-  alias(libs.plugins.secrets)
 }
 
 android {
@@ -57,14 +56,8 @@ android {
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
 }
-
-// Configure the Secrets Gradle Plugin to use .env and .env.example files
-// to match the convention used in Web projects.
-secrets {
-  propertiesFileName = ".env"
-  defaultPropertiesFileName = ".env.example"
-}
-
+// Secrets plugin removed to allow keyless production build
+android {
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
 dependencies {
